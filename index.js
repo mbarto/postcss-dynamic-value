ce64bd1c42fa03841f6d5a25774bed42f6986cd6
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2023-01-22 15:09:25 +0100

    improved selector recognition by using a regex

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+const searchDynamicValueSelector = /:\[value(.*?)\]/gi
+const dynamicAttribute = "data-postcss-dynamic-value"
+
 const plugin = () => {
   return {
     postcssPlugin: "postcss-dynamic-value",
 
     Once (css) {
       css.walkRules(rule => {
-        if (rule.selector && rule.selector.indexOf(":[value") !== -1) {
-          rule.selector = rule.selector.replaceAll(
-            ':[value',
-            '[data-postcss-dynamic-value'
+        if (
+          rule.selector &&
+          rule.selector.match(searchDynamicValueSelector) !== -1
+        ) {
+          rule.selector = rule.selector.replace(
+            searchDynamicValueSelector,
+            `[${dynamicAttribute}$1]`
           )
         }
       })
--- index.test.js
+++ index.test.js
@@ -1,6 +1,6 @@
 import postcss from 'postcss'
 
-import plugin from '.'
+import plugin from './index'
 
 async function run (input, output, opts = { }) {
   let result = await postcss([plugin(opts)]).process(input, { from: undefined })
@@ -10,4 +10,15 @@ async function run (input, output, opts = { }) {
 
 it('valid dynamic value selector', async () => {
   await run('input:[value="test"] {}', 'input[data-postcss-dynamic-value="test"] {}')
+})
+
+it('invalid dynamic value selector is not changed', async () => {
+  await run('input:[dummy="test"] {}', 'input:[dummy="test"] {}')
+})
+
+it('dynamic value selector with other operator', async () => {
+  await run(
+    'input:[value$="test"] {}',
+    'input[data-postcss-dynamic-value$="test"] {}'
+  )
 })
